d365eedf7aaaefece53bec3a4683afaa8a94aa35
BriannaD23 <briannadarladuncan@gmail.com>
2023-03-16 07:44:27 -0700

    master

---
+++ dist/script.js
@@ -0,0 +1,53 @@
+var tabLinks= document.getElementsByClassName("tab-links")
+var tabContents= document.getElementsByClassName("tab-contents")
+
+function opentab(tabname){
+  for(tablink of tabLinks){
+      tablink.classList.remove("active-link");
+  }
+  
+  for(tabcontent of tabContents){
+      tabcontent.classList.remove("active-tab");
+   }
+   event.currentTarget.classList.add("active-link");
+   document.getElementById(tabname).classList.add("active-tab");
+  
+}
+
+
+
+var sideMenu = document.getElementById("side-menu")
+
+function openMenu(){
+    sideMenu.style.right="0"
+}
+
+function closeMenu(){
+    sideMenu.style.right="-200px"
+}
+
+function submitForm(){
+ 
+}
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwU1H2eaz13jgR0o9sWuGkn8Gz6x5QQ-tLiwlwVqbyMBIkAeZy6kGUMtFGuSM4-h9rt/exec'
+
+const form = document.forms['submit-to-google-sheet']
+
+const msg= document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML= "Message Sent Successfully!"
+        setTimeout(function(){
+            msg.innerHTML= ""
+        }, 3000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+})
